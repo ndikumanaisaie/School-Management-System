b565c5f40fd0f95842d78077fb00e15931ff0ce5
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-06-09 07:55:44 +0300

    Add register page

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import Register from '/src/pages/Register.jsx'
+// import { useDispatch } from 'react-redux';
+// import { Routes, Route } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// import './app.css';
+// import Home from './pages/Home.jsx';
+// import Login from './pages/Login.jsx';
+// import Register from './pages/Register.jsx';
+// import Post from './pages/Post.jsx';
+// import Header from './components/Header.jsx';
+// import SinglePost from './pages/SinglePost.jsx';
+// import { setUser } from './features/authSlice.js';
+// import Dashboard from './pages/Dashboard.jsx';
+// import PrivateRoute from './components/PrivateRoute.jsx';
+// import NotFound from './pages/NotFound.jsx';
+// import TagPosts from './pages/TagPosts.jsx';
+// import ReactBootstrapNav from './components/ReactBootstrapNav.jsx';
+
+const App = () => {
+	// const [activeMenu, setActiveMenu] = useState(true);
+	// const [screenSize, setScreenSize] = useState(null);
+
+	// const dispatch = useDispatch();
+	// const user = JSON.parse(localStorage.getItem('profile'));
+
+	// useEffect(() => {
+	// 	const handleResize = () => setScreenSize(window.innerWidth);
+
+	// 	window.addEventListener('resize', handleResize);
+
+	// 	handleResize();
+
+	// 	return () => window.removeEventListener('resize', handleResize);
+	// }, []);
+
+	// useEffect(() => {
+	// 	if (screenSize < 768) {
+	// 		setActiveMenu(false);
+	// 	} else {
+	// 		setActiveMenu(true);
+	// 	}
+	// }, [screenSize]);
+
+	// useEffect(() => {
+	// 	dispatch(setUser(user));
+	// });
+
+	return (
+		<div className='app' id="outer-container">
+			 <div id="page-wrap">
+				<Register />
+				{/* <Header />
+				<ToastContainer />
+				<Routes>
+					<Route path='/' element={ <Home />} />
+					<Route path='/post/search' element={ <Home />} />
+					<Route path='/post/tag/:tag' element={ <TagPosts />} />
+					<Route path='/login' element={ <Login />} />
+					<Route path='/register' element={ <Register />} />
+					<Route
+						path='/post'
+						element={
+							<PrivateRoute>
+								<Post />
+							</PrivateRoute>
+						} />
+					<Route
+						path='/editPost/:id'
+						element={
+							<PrivateRoute>
+								<Post />
+							</PrivateRoute>
+						} />
+					<Route path='/post/:id' element={ <SinglePost />} />
+					<Route
+						path='/dashboard'
+						element={
+							<PrivateRoute>
+								<Dashboard />
+							</PrivateRoute>
+						} />
+					<Route path='*' element={<NotFound />}/>
+				</Routes> */}
+			 </div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import App from './App.js';
+// import authReducer from './features/authSlice.js';
+// import postReducer from './features/postSlice.js';
+
+// const store = configureStore({
+// 	reducer: {
+// 		auth: authReducer,
+// 		posts: postReducer,
+// 	},
+// });
+const root = createRoot(document.getElementById('root'));
+root.render(
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>,
+);
